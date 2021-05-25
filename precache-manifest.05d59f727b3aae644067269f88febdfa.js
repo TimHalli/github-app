99e57ea29e809df5b8fdbe653493f7da2d11905e
tim halli <t.timhalli@gmail.com>
2021-05-25 07:21:31 +0300

    Updates

---
+++ precache-manifest.05d59f727b3aae644067269f88febdfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8af60ca46a8e441ac828273c854e18",
+    "url": "/github-app/index.html"
+  },
+  {
+    "revision": "aa7543ca13041ad59af2",
+    "url": "/github-app/static/css/main.ceb9dfde.chunk.css"
+  },
+  {
+    "revision": "079be8be7b753eebd0c2",
+    "url": "/github-app/static/js/2.d60c1722.chunk.js"
+  },
+  {
+    "revision": "aa7543ca13041ad59af2",
+    "url": "/github-app/static/js/main.b3b07a98.chunk.js"
+  },
+  {
+    "revision": "565b4aa1b8aaaa4f3036",
+    "url": "/github-app/static/js/runtime~main.d15fc806.js"
+  }
+]);
